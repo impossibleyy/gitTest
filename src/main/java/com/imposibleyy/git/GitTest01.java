13eb2b812ff5c6e2ebd1674dcc2bf6ffef00ecba
YuSong <YuSong@163.com>
2022-05-05 20:51:01 +0800

    third commit

--- src/main/java/com/imposibleyy/git/GitTest01.java
+++ src/main/java/com/imposibleyy/git/GitTest01.java
@@ -2,7 +2,8 @@ package com.imposibleyy.git;
 
 public class GitTest01 {
     public static void main(String[] args) {
-        System.out.println("这是第二个idea git项目！！！");
-        System.out.println("这是第二个idea git项目！！！");
+        System.out.println("这是第3个idea git项目！！！");
+        System.out.println("这是第3个idea git项目！！！");
+        System.out.println("这是第3个idea git项目！！！");
     }
 }
