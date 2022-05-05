1e047304bba17fea7608669cbb91311d7b7de70d
YuSong <YuSong@163.com>
2022-05-05 20:49:20 +0800

    first commit

--- src/main/java/com/imposibleyy/git/GitTest01.java
+++ src/main/java/com/imposibleyy/git/GitTest01.java
@@ -2,6 +2,7 @@ package com.imposibleyy.git;
 
 public class GitTest01 {
     public static void main(String[] args) {
-        System.out.println("这是第一个idea git项目！！！");
+        System.out.println("这是第二个idea git项目！！！");
+        System.out.println("这是第二个idea git项目！！！");
     }
 }
